980cc55bcfac7af7e647823af6130f2594c887b9
Seth Keirn <118314693+sethorater@users.noreply.github.com>
2025-09-14 21:33:09 -0600

    Update PasswordCheck.js

--- Assignment3_9_14_25/PasswordCheck.js
+++ Assignment3_9_14_25/PasswordCheck.js
@@ -3,6 +3,11 @@ function isStrongPassword(password) {
         console.log("too short");
         return false;
     }
+    if (password.includes("password") || password.includes("1234")) {
+        console.log("has either password or 1234");
+        return false;
+    }
 }
 
 isStrongPassword("qwerty1");
+isStrongPassword("qwertypassword1");
